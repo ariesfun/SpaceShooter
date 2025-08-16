02f88e7f030fb7aa3df18401eb570d991bf5f6d3
ariesfun <ariesfun2019@outlook.com>
2025-08-16 12:10:27 +0800

    feat: init repo

---
+++ build.bat
@@ -0,0 +1,6 @@
+rm build -r
+mkdir build
+cd build
+cmake .. -DCMAKE_BUILD_TYPE=Release
+cmake --build . --config Release
+cd ..
---
+++ game/test.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main() 
+{
+    std::cout << "Hello, 2025!" << std::endl;
+    return 0;
+}
