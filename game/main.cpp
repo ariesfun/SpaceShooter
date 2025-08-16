4e7ce64379098b68c2018b8ae956541f8e73d2ae
ariesfun <ariesfun2019@outlook.com>
2025-08-16 14:52:07 +0800

    feat: import sdl lbs and test use

---
+++ game/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <SDL.h>
+
+int main(int argc, char* argv[]) // 注意：这里是SDL2要求的入口函数形式
+{
+    std::cout << "Hello, SDL2!" << std::endl;
+    return 0;
+}
--- game/test.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-
-int main() 
-{
-    std::cout << "Hello, 2025!" << std::endl;
-    return 0;
-}
